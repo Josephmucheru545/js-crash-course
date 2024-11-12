d35852fb65464a475ff718cde47d9d84af4de3b3
Josephmucheru545 <157952964+Josephmucheru545@users.noreply.github.com>
2024-11-12 14:59:40 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,87 @@
+
+// x=100;
+// y=30;
+
+
+// console.log(x%y)
+// console.log(++x)
+// console.log(++y)
+// console.log(x+y)
+// console.log('joseph ' + 'mucheru')
+// const isEven = 20 % 0=== ? 'number is even': 'number is odd'
+// console.log(isEven)
+
+// console.log('3'-'5'+'true')
+// console.log(4*8)
+// console.log('5'-'2')
+// console.log('5'+'2')
+// console.log('5'%'2')
+// console.log('5'/'2')
+// console.log('5'+'2')
+// console.log(2+3)
+// console.log(2-3)
+// console.log(2*3)
+// console.log(9%3)
+// console.log(9/3)
+
+// console.log('joseph'+'mucheru')
+// console.log('joseph'-'mucheru')
+// console.log('joseph'/'mucheru')
+// console.log('joseph'%'mucheru')
+// console.log('joseph'*'mucheru')
+// console.log('1'+'3')
+// console.log(5+true);
+// console.log('5'% undefined);
+
+// console.log(Number(5));
+// console.log(parseInt(5));
+// console.log(parseFloat(5.765));
+// console.log(String(500));
+// console.log(Boolean(50));
+
+
+
+
+
+// 
+// const num1=0;
+
+// if (num1>0){
+//     console.log('num1 is positive');
+    
+        
+    
+// }
+// else if(num1<0){
+//     console.log('num1 is an negative');
+
+    
+// }
+// else{
+//     console.log('num1 is zero');
+    
+// }
+
+
+// const name='koech'
+// switch(name){
+//     case 'kamau':
+//         console.log('kamau is my name');
+//         break
+//         case 'mwangi':
+//         console.log('i am mwangi');
+//         break
+//         case 'mucheru':
+//         console.log('i go by the name mucheru');
+//         break
+//         default:
+//             console.log('none of the above');
+            
+        
+// }
+// let i =1;
+// do{
+//     console.log('iteration number'+i)
+//     i++;
+// }
+// while(i<=10)
